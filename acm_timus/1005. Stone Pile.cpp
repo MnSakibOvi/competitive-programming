b42cc6daaef4223cc995ccfb3abd0061cab50050
MnSakibOvi <mnsakib1812@gmail.com>
2021-06-30 20:47:46 +0600

    practice

--- acm_timus/1005. Stone Pile.cpp
+++ acm_timus/1005. Stone Pile.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main() {
-    int n,ans;
-    int arr[22];
+    int n,ans=INT_MAX,an;
+    int arr[21];
     int sum = 0;
 
     scanf("%d", &n);
@@ -12,31 +12,18 @@ int main() {
         scanf("%d", &arr[i]);
         sum += arr[i];
     }
-int sum1=0;
-   for(int i=0;i<n;i++)
-   {
-       int mn=INT_MAX,k=0;
-       for(int j=0;j<n;j++)
-       {
-           if(arr[j]>=0)
-           {
-             int g=abs((2*arr[i])-sum);
-             if(g<mn)
-             {
-                 mn=g;
-                 k=j;
 
-
-             }
-           }
-       }
-       sum-=arr[k];
-       sum1+=arr[k];
-       arr[k]=-1;
-       cout<<sum<<" "<<sum1<<endl;
-   }
-   ans=abs(sum-sum1);
-
-    printf("%d\n",ans);
+  for(int i=0;i<(1<<n);i++)
+  {
+      int m=0;
+      for(int j= 0;j<n;j++)
+      {
+          if(i&(1<<j))
+            m+=arr[j];
+      }
+      an=abs(2*m-sum);
+      ans=min(an,ans);
+  }
+  cout<<ans<<endl;
     return 0;
 }
--- light OJ/Hex-a-bonacci.cpp
+++ light OJ/Hex-a-bonacci.cpp
@@ -1,1 +1,28 @@
 
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int t;
+cin>>t;
+for(int cas=1;cas<=t;cas++)
+{
+  long long arr[10003],n,mod=10000007;
+  for(int i=0;i<6;i++)
+  {
+      cin>>arr[i];
+  }
+  cin>>n;
+  for(int i=6;i<=n;i++)
+  {
+      arr[i]=(arr[i-1]+arr[i-2]+arr[i-3]+arr[i-4]+arr[i-5]+arr[i-6])%mod;
+  }
+  cout<<"Case "<<cas<<": "<<arr[n]%mod<<endl;
+}
+
+    return 0;
+}
+
